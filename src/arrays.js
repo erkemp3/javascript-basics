9ba5c2cbc911f23247693b887785c64ce7c2b0f8
Ellie Kemp <elliekemp95@gmail.com>
2021-03-10 15:17:58 +0000

    adding solutions to objects and arrays

--- src/arrays.js
+++ src/arrays.js
@@ -1,61 +1,84 @@
 const getNthElement = (index, array) => {
-  // your code here
+  return array[index % array.length];
 };
 
-const arrayToCSVString = array => {
-  // your code here
+const arrayToCSVString = (array) => {
+  return (array.toString());
 };
 
-const csvStringToArray = string => {
-  // your code here
+const csvStringToArray = (string) => {
+  return (string.split(","));
 };
 
 const addToArray = (element, array) => {
-  // your code here
+  array.push(element);
 };
 
 const addToArray2 = (element, array) => {
-  // your code here
+ return(array.concat(element));
+
 };
 
 const removeNthElement = (index, array) => {
-  // your code here
+  return array.splice(index, 1);
 };
 
-const numbersToStrings = numbers => {
-  // your code here
+const numbersToStrings = (numbers) => {
+  return numbers.toString().split(",");
 };
 
 const uppercaseWordsInArray = strings => {
-  // your code here
+  const uppercased = strings.map(string => string.toUpperCase());
+  return uppercased; 
 };
 
 const reverseWordsInArray = strings => {
-  // your code here
+  const reversed = strings.map(string => string.split("").reverse().join(""));
+  return reversed;
 };
 
 const onlyEven = numbers => {
-  // your code here
+  const evens = [];
+  for (let i=0; i < numbers.length; i += 1) {
+  if (numbers[i] % 2 === 0) {
+  evens.push(numbers[i]);
+  }
+  }
+  return evens
 };
 
 const removeNthElement2 = (index, array) => {
-  // your code here
+  const newArray = [...array];
+  newArray.splice(index, 1);
+  return newArray;
 };
 
 const elementsStartingWithAVowel = strings => {
-  // your code here
+  return strings.filter(str => /^[aeiou]/i.test(str));
 };
 
-const removeSpaces = string => {
-  // your code here
+
+const removeSpaces = (string) => {
+  const removed = string.split(" ").join("");
+  return removed
 };
 
 const sumNumbers = numbers => {
-  // your code here
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i += 1) {
+  sum += numbers[i];
+}
+return sum
 };
 
 const sortByLastLetter = strings => {
-  // your code here
+  
+function endComparator(a,b) {
+    if (a.slice(-1) < b.slice(-1)) return -1;
+    if (a.slice(-1) > b.slice(-1)) return 1;
+    return 0;
+}
+return strings.sort(endComparator);
 };
 
 module.exports = {
